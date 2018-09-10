88563c9b9921b8e7802c058e1cdd6ed4e4807016
dhx448690417 <448690417@qq.com>
2018-09-10 17:48:24 +0800

    123

---
+++ application/common/controller/Base.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\common\controller;
+
+use think\Controller;
+
+class Base extends Controller
+{
+	
+
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\index\controller;
+use app\common\controller\Base;
+class Index extends Base
+{
+    public function index()
+    {
+        return $this->fetch('header');
+    }
+}
